fed344101d48a240944c5d68ee7e3e7793bd5e51
Stanislav Romanov <kaermorchen@gmail.com>
2024-05-26 11:10:56 +0300

    Add domain locales

--- src/context_menu.js
+++ src/context_menu.js
@@ -4,36 +4,47 @@ browser.contextMenus.create({
   onclick: lookUpYandexTranslate,
 });
 
-function lookUpYandexTranslate(info) {
-  if (info) {
-    browser.storage.sync
-      .get({
-        window_height: 650,
-        window_width: 900,
-        target: "window",
-      })
-      .then(function (result) {
-        const url = "https://translate.yandex.ru/?text=" + info.selectionText;
+const domainLocales = new Map([
+  ["en", "com"],
+  ["ru", "ru"],
+]);
 
-        if (result.target === "tab") {
-          browser.tabs.create({
-            url: url,
-          });
-        } else {
-          const w = result.window_width;
-          const h = result.window_height;
-          const left = screen.width / 2 - w / 2;
-          const top = screen.height / 2 - h / 2;
+async function lookUpYandexTranslate(info) {
+  if (!info) {
+    return;
+  }
 
-          browser.windows.create({
-            url: url,
-            type: "popup",
-            width: w,
-            height: h,
-            left: left,
-            top: top,
-          });
-        }
-      }, console.error);
+  const gettingAcceptLanguages = await browser.i18n.getAcceptLanguages();
+
+  const supportedLanguage =
+    gettingAcceptLanguages.find((item) => domainLocales.has(item)) || "en";
+
+  const domainLocale = domainLocales.get(supportedLanguage);
+  const url = `https://translate.yandex.${domainLocale}/?text=${info.selectionText}`;
+
+  const settings = await browser.storage.sync.get({
+    window_height: 650,
+    window_width: 900,
+    target: "window",
+  });
+
+  if (settings.target === "tab") {
+    browser.tabs.create({
+      url: url,
+    });
+  } else {
+    const w = settings.window_width;
+    const h = settings.window_height;
+    const left = screen.width / 2 - w / 2;
+    const top = screen.height / 2 - h / 2;
+
+    browser.windows.create({
+      url: url,
+      type: "popup",
+      width: w,
+      height: h,
+      left: left,
+      top: top,
+    });
   }
 }
